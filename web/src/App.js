dfe9674cd59c68e00e8bcb41eb053f01f32b8691
KallicoFrois <froiskallico@gmail.com>
2020-01-16 17:05:29 -0300

    build: frontend web

--- web/src/App.js
+++ web/src/App.js
@@ -1,50 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
+
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
 
 // Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação;
 // Propriedade: Informações que um componente PAI passa para um componente FILHO. i.e: <Header title="Titulo 1" />. Usa-se a propriedade props.title dentro do componente Header.js;
 // Estado: Informações mantidas pelo componente (Lembrar: imutabilidade); Para usar usa useState.
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);  
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input_block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required></input>
-          </div>
-
-          <div className="input_block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required></input>
-          </div>
-
-          <div className="input_block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required></input>
-          </div>
-
-          <div className="input-group">
-            <div className="input_block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required></input>
-            </div>
-            
-            <div className="input_block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required></input>
-            </div>
-          </div>
-
-        </form>
+        <DevForm onSubmit={handleAddDev} />
+        
       </aside>
       <main>
-
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+          
+        </ul>
       </main>
     </div>
   );
